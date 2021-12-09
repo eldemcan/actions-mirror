3bf579e2d7b8472b294ffdd8e1822871b7ac3221
Can Eldem <eldemcan@users.noreply.github.com>
2021-12-09 22:59:08 +0000

    y5

--- .github/workflows/discord.sh
+++ .github/workflows/discord.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 set -e
-echo "Running...."
 
-API_WEB=false
-HOSTING=false
-API_DEPLOY=true
-SHA=123
-TITLE=can
-HOOK="https://discord.com/api/webhooks/909939146238341181/btXG0UeWa7XljQRd3TOdBt9D4ezDB6C3K2h5yv9nVTeTab6mKE6ht1DS4erfSZEPEfcR"
-
+if [ "$MANUAL" == true ]; then
+curl --request POST \
+  --url $HOOK \
+  --header 'Content-Type: application/json' \
+  --data '{
+  "embeds": [
+    {
+      "title": "'$TITLE'",
+      "fields": [
+        {
+          "name": "Release Tag",
+          "value": "'$RELEASE_TAG'"
+        },
+        {
+          "name": "Service",
+          "value": "'$SERVICE'"
+        },
+        {
+          "name": "Environment",
+          "value": "'$ENVIRONMENT'"
+        },
+        {
+          "name": "Ansible Tag",
+          "value": "'$ANSIBLE_TAG'"
+        },
+        {
+          "name": "Success?",
+          "value": "'$OUTCOME'"
+        }
+      ]
+    }
+  ]
+}'
+else
 curl --request POST \
   --url $HOOK \
   --header 'Content-Type: application/json' \
@@ -37,3 +63,4 @@ curl --request POST \
     }
   ]
 }'
+fi
