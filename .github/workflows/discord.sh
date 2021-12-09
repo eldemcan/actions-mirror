1f27650f399a443d9616ddbe0be533f26c6c7d1a
Can Eldem <eldemcan@users.noreply.github.com>
2021-12-09 21:37:49 +0000

    Create discord.sh

---
+++ .github/workflows/discord.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl --request POST \
+--url $HOOK \
+--header 'Content-Type: application/json' \
+--data '{
+ "embeds": [
+   {
+     "title": "Deployment Status",
+     "fields": [
+       {
+         "name": "Sha",
+         "value": ${NAME},
+       },
+       {
+         "name": "Api Web",
+         "value": "https://vsk.me/",
+       },
+       {
+         "name": "Api DeployExit code",
+         "value": "0",
+       },
+       {
+         "name": "Hosting",
+         "value": "true"
+       }
+     ]
+   }
+ ]
+}'
